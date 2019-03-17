625f101eff2320f80aa0a1b39996817da6975610
Marcel Widmer <marzelwidmer@gmail.com>
2019-03-17 09:44:32 +0100

    Coroutine and Without Coroutine

--- src/main/kotlin/ch/c3smonkey/sample/kotlin/coroutines/VenkatDemo.kt
+++ src/main/kotlin/ch/c3smonkey/sample/kotlin/coroutines/VenkatDemo.kt
@@ -1,21 +1,44 @@
 package ch.c3smonkey.sample.kotlin.coroutines
 
+import kotlinx.coroutines.*
 
 fun main() {
+    println("################# Without Coroutine ")
+    callRemoteJokeService(amount = 100)
+    println("################# Coroutine ")
+    callRemoteJokeServiceWithCoroutine(amount = 100)
+
+}
+
+private fun callRemoteJokeService(amount: Int) {
     measureTime {
-        val jokes = processJoke(amount = 100)
+        val jokes = mutableListOf<String>()
+        for (i in 1..amount) {
+            jokes += getJoke()
+        }
         println("Found ${jokes.size} jokes.")
         println("request from ${getCallerIP()}")
     }
 }
 
-private fun processJoke(amount: Int): MutableList<String> {
-    val jokes = mutableListOf<String>()
-    for (i in 1..amount) {
-        jokes += getJoke()
-    }
 
-    return jokes
+private fun callRemoteJokeServiceWithCoroutine(amount: Int) {
+    measureTime {
+        val jokes = mutableListOf<Deferred<String>>()
+        // Start a coroutine
+        GlobalScope.launch {
+            for (i in 1..amount) {
+                jokes += async { getJoke() }
+            }
+        }
+        //
+        runBlocking {
+            jokes.awaitAll<String>()
+
+        }
+        println("Found ${jokes.size} jokes.")
+        println("request from ${getCallerIP()}")
+    }
 }
 
 
